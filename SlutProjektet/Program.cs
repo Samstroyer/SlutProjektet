4e64a4f8db1df85550b4be3db41907150c03dd7b
Samuel Palmér <samiskramis@gmail.com>
2022-03-22 13:06:40 +0100

    First commit to commit to the slutprojekt

---
+++ SlutProjektet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Raylib_cs;
+
+/*
+    Game created (soon) by Samuel Palmér.
+
+    This game is called ---- and it is inspired by ----.
+    Created for Slut-Projektet.
+*/
+
+
+Game();
+
+void Game()
+{
+    Console.WriteLine("Hello I guess!");
+}
