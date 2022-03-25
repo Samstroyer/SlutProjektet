1c359903c74ad59e93dc064f4b028b32ff363fff
Samuel Palmér <samiskramis@gmail.com>
2022-03-25 08:22:04 +0100

    MenuTemplate är klar Börjar navigeringslogik

---
+++ SlutProjektet/MenuTemplate.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using Raylib_cs;
+
+class Menu
+{
+    string name;
+
+    //Jag behöver inte hastighet i en "menu", och att arbeta med en 
+    //dynamisk lista gör det mycket enklare än en "statisk" array 
+    public List<(Rectangle shape, string prompt)> menuButtons = new List<(Rectangle shape, string prompt)>();
+
+    public Menu(string requestedName)
+    {
+        name = requestedName;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public string Run()
+    {
+        string option = "stay";
+        
+        foreach ((Rectangle, string) b in menuButtons)
+        {
+            Display(b);
+            option = Hover(b);
+            if (option != "stay")
+            {
+                return option;
+            }
+        }
+
+        System.Console.WriteLine(option);
+        return option;
+    }
+
+    private void Display((Rectangle shape, string prompt) button)
+    {
+        Raylib.DrawRectangleRec(button.Item1, Color.RED);
+        Raylib.DrawRectangleLinesEx(button.Item1, 10, Color.GRAY);
+        Raylib.DrawText(button.Item2, (int)button.Item1.x + 20, (int)(button.Item1.y + button.Item1.height / 3), 60, Color.BLACK);
+    }
+
+    private string Hover((Rectangle shape, string prompt) button)
+    {
+        string option = "stay";
+        var mouseCords = Raylib.GetMousePosition();
+
+        if (mouseCords.X > button.shape.x && mouseCords.X < button.shape.x + button.shape.width)
+        {
+            if (mouseCords.Y > button.shape.y && mouseCords.Y < button.shape.y + button.shape.height)
+            {
+                Raylib.DrawRectangleRec(button.shape, Color.DARKGRAY);
+                Raylib.DrawRectangleLinesEx(button.shape, 10, Color.BLACK);
+                Raylib.DrawText(button.prompt, (int)button.shape.x + 20, (int)(button.shape.y + button.shape.height / 3), 60, Color.BLACK);
+
+                if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
+                {
+                    return button.prompt;
+                }
+            }
+        }
+
+        return option;
+    }
+}
--- SlutProjektet/Program.cs
+++ SlutProjektet/Program.cs
@@ -1,17 +1,54 @@
-﻿using System;
-using Raylib_cs;
+﻿using Raylib_cs;
+using System.Collections.Generic;
 
 /*
     Game created (soon) by Samuel Palmér.
 
-    This game is called ---- and it is inspired by ----.
-    Created for Slut-Projektet.
+    This game is a collection of Text-Or-Die and Wordle and it is inspired by my phone games and people playing Wordle.
+    I decided to program both as I have been trying to decide what game to do, but I really can't...
+
+    This was created for my assignment : "Slutprojektet".
 */
 
+Menu currentScreen;
+string currentMenu;
+Menu startScreen;
 
+Setup();
 Game();
+
+void Setup()
+{
+    Raylib.InitWindow(1000, 800, "S-GameLauncher");
+    Raylib.SetTargetFPS(60);
+
+    startScreen = new Menu("StartScreen")
+    {
+        menuButtons = new List<(Rectangle shape, string prompt)> {
+            (new Rectangle(300, 125, 400, 150), "Start Game"),
+            (new Rectangle(300, 325, 400, 150), "Settings"),
+            (new Rectangle(300, 525, 400, 150), "Quit")
+        }
+    };
+    currentScreen = startScreen;
+    currentMenu = currentScreen.GetName();
+}
 
 void Game()
 {
-    Console.WriteLine("Hello I guess!");
+    while (!Raylib.WindowShouldClose())
+    {
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.WHITE);
+
+        switch (currentMenu)
+        {
+            case "stay":
+                currentScreen.Run();
+                break;
+        }
+
+
+        Raylib.EndDrawing();
+    }
 }
